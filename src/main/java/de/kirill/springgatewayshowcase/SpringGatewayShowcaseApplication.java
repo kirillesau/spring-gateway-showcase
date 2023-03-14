ca93b9b266472e8b81b220a852bc58494b9c7c8a
kirill.esau <kirill.esau@adesso.de>
2023-03-14 10:00:37 +0100

    init

---
+++ src/main/java/de/kirill/springgatewayshowcase/SpringGatewayShowcaseApplication.java
@@ -0,0 +1,13 @@
+package de.kirill.springgatewayshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayShowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kirill/springgatewayshowcase/SpringGatewayShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kirill.springgatewayshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
